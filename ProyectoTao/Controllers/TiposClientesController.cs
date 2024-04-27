693a7cf3f6e666629895748c5fe616e134b2a8ce
Espadas01 <yenni.medina@ventustechnology.onmicrosoft.com>
2024-04-26 22:09:33 -0600

    first commit

---
+++ ProyectoTao/Controllers/TiposClientesController.cs
@@ -0,0 +1,28 @@
+﻿using ProyectoTao.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoTao.Controllers
+{
+
+
+    public class TiposClientesController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCliente tipoCliente) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCliente);
+            }
+            return View();
+        }
+
+
+
+    }
+}
---
+++ ProyectoTao/Models/TipoCliente.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTao.Models
+{
+    public class TipoCliente
+    {
+        public int ClaveCliente { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]
+        
+    public string IDCliente { get; set; }
+        
+
+        public DateTime FechaAlta { get; set; }
+        
+
+        public int TiempoInactivo { get; set; }
+        
+
+        public bool Estatus { get; set; }
+        
+
+    }
+}
